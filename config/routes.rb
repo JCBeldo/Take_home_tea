ad8734a56961dadc6e6d3be75b8688dc04a7ec65
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-13 22:34:45 -0600

    Feat: add create into routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index]
+      resources :customers, only: [:index, :create]
     end
   end
 end
