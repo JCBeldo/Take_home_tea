6b26018e28088f558a8be6b2a13c804a5454f7f2
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-13 21:41:31 -0600

    Feat: Add join tables and update schema

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,4 @@
 class Tea < ApplicationRecord
-  has_many :subscriptions, dependent: :destroy
+  has_many :tea_subscriptions, dependent: :destroy
+  has_many :subscriptions, through: :tea_subscriptions
 end
