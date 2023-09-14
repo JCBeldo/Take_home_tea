639ee465372201d04df8f627511a6a4178ded302
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-14 17:29:45 -0600

    Feat: Update routes to have subscription nested under customers

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index, :create]
+      resources :customers, only: [:index, :create] do
+        resources :subscriptions, only: [:index]
+      end
       resources :subscriptions, only: [:index, :create]
     end
   end
