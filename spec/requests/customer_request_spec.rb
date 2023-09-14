86d8b2d3c34053f04ca6a620d22aff9020d157dd
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-13 22:13:31 -0600

    Test: Add request spec for customer endpoint

---
+++ spec/requests/customer_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'Customer API' do
+  it 'sends a list of customers' do
+    create_list(:customer, 3)
+
+    get '/api/v1/customers'
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    customers_json = JSON.parse(response.body, symbolize_names: true)
+    customers = customers_json[:data]
+
+    expect(customers.count).to eq(3)
+
+    customers.each do |customer|
+      expect(customer).to have_key(:id)
+      expect(customer[:id]).to be_an(String)
+
+      expect(customer).to have_key(:type)
+      expect(customer[:type]).to eq('customer')
+
+      expect(customer).to have_key(:attributes)
+      expect(customer[:attributes]).to be_a(Hash)
+
+      expect(customer[:attributes]).to have_key(:first_name)
+      expect(customer[:attributes][:first_name]).to be_a(String)
+
+      expect(customer[:attributes]).to have_key(:last_name)
+      expect(customer[:attributes][:last_name]).to be_a(String)
+
+      expect(customer[:attributes]).to have_key(:email)
+      expect(customer[:attributes][:email]).to be_a(String)
+
+      expect(customer[:attributes]).to have_key(:address)
+      expect(customer[:attributes][:address]).to be_a(String)
+    end
+  end
+end
