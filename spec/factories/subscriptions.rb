093943943f9e63fa63cf607bc796bd08aa72dac5
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-14 11:59:31 -0600

    Test: Update factory for enum

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :subscription do
     title  { Faker::Subscription.plan }
     price  { Faker::Number.between(from: 20, to: 100) }
-    status { Faker::Subscription.status }
+    # status { Faker::Subscription.status }
+    status { Faker::Number.between(from: 0, to: 1) }
     frequency { Faker::Subscription.subscription_term }
   end
 end
