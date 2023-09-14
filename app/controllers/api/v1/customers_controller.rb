286cfbcfdfc4d6a976c32e0add699d4d5af31d81
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-14 11:57:42 -0600

    Feat: Add actions for subscription controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,12 +4,17 @@ class Api::V1::CustomersController < ApplicationController
   end
 
   def create
-    render json: CustomerSerializer.new(Customer.create(customer_params))
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: CustomerSerializer.new(Customer.create(customer_params), status: 201)
+    else
+      render json: { error: customer.errors.full_messages.to_sentence }, status: 400
+    end
   end
 
   private
 
   def customer_params
-    params.permit(:first_name, :last_name, :email, :address)
+    params.require(:customer).permit(:first_name, :last_name, :email, :address)
   end
 end
---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def index
+    render json: SubscriptionSerializer.new(Subscription.all)
+  end
+
+  def create
+    subscription = Subscription.new(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(Subscription.create(subscription_params), status: 201)
+    else
+      render json: { error: subscription.errors.full_messages.to_sentence }, status: 400
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :price, :status, :frequency)
+  end
+end
