9c140321f0c3e8ff829d4695540c1948ba4a626c
Julian Beldotti <julian.beldotti@gmail.com>
2023-09-13 22:34:13 -0600

    Feat: Add create method to controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::CustomersController < ApplicationController
   def index
     render json: CustomerSerializer.new(Customer.all)
   end
+
+  def create
+    render json: CustomerSerializer.new(Customer.create(customer_params))
+  end
+
+  private
+
+  def customer_params
+    params.permit(:first_name, :last_name, :email, :address)
+  end
 end
